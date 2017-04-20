9ad1c4ac2e373100760917f28c9094d5084bb5f1
Aktorius <miri.tarik@gmail.com>
2017-04-20 22:41:56 +0100

    Create Employee interface

---
+++ src/main/java/aktorius/com/interfaces/Employee.java
@@ -0,0 +1,12 @@
+package main.java.aktorius.com.interfaces;
+
+/**
+ * Created by Aktorius on 20/04/2017.
+ */
+public interface Employee {
+
+    public String getId();
+    public String getFirstName();
+    public String getLastName();
+    public String getEmail();
+}
