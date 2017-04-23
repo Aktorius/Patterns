827f93d482a22abd93518aeca2e3bd4d9b96d856
Aktorius <miri.tarik@gmail.com>
2017-04-23 12:25:32 +0100

    test MovieBridge

--- src/main/java/aktorius/com/application/PatternsDemo.java
+++ src/main/java/aktorius/com/application/PatternsDemo.java
@@ -1,10 +1,10 @@
 package main.java.aktorius.com.application;
 
-
-import abstracts.Shape;
-import colors.*;
-import interfaces.Color;
-import shapes.*;
+import abstracts.Printer;
+import domain.*;
+import interfaces.Formatter;
+import services.MoviePrinter;
+import services.PrintFormatter;
 
 /**
  * Created by Aktorius on 20/04/2017.
@@ -12,18 +12,17 @@ import shapes.*;
 public class PatternsDemo {
 
     public static void main (String[] args){
-        Color blue = new Blue();
-        Color red = new Red();
-        Color green = new Green();
+        Movie movie = new Movie();
+        movie.setClassification("Action");
+        movie.setTitle("John Wick");
+        movie.setRuntime("2:15");
+        movie.setYear("2014");
 
-        Shape square = new Square(blue);
-        Shape circle = new Circle(red);
-        Shape greenCircle = new Circle(green);
-        Shape greenSquare = new Square(green);
+        Formatter printFormatter = new PrintFormatter();
+        Printer moviePrinter = new MoviePrinter(movie);
 
-        square.applyColor();
-        circle.applyColor();
-        greenCircle.applyColor();
-        greenSquare.applyColor();
+        String printedMaterial = moviePrinter.print(printFormatter);
+
+        System.out.println(printedMaterial);
     }
 }
