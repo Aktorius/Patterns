4d7ad5c20e28c0d616a6b386e60738eece1918ae
Aktorius <miri.tarik@gmail.com>
2017-04-24 23:03:49 +0100

    Create domain foor the Prototype pattern

---
+++ Prototype/src/domain/Book.java
@@ -0,0 +1,17 @@
+package domain;
+
+/**
+ * Created by Aktorius on 24/04/2017.
+ */
+public class Book extends Item {
+
+    private int numberOfPages;
+
+    public int getNumberOfPages() {
+        return numberOfPages;
+    }
+
+    public void setNumberOfPages(int numberOfPages) {
+        this.numberOfPages = numberOfPages;
+    }
+}
---
+++ Prototype/src/domain/Movie.java
@@ -0,0 +1,17 @@
+package domain;
+
+/**
+ * Created by Aktorius on 24/04/2017.
+ */
+public class Movie extends Item {
+
+    private String runtime;
+
+    public String getRuntime() {
+        return runtime;
+    }
+
+    public void setRuntime(String runtime) {
+        this.runtime = runtime;
+    }
+}
